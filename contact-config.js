878cae8bb8b8fb389e891ef48bf7ca2edd54433f
joanseu3234-dotcom <joanseu3234@gmail.com>
2026-04-20 14:10:01 +0800

    🔄 後台發布：2026/4/20 14:09:56

---
+++ contact-config.js
@@ -0,0 +1,9 @@
+// IPCC 聯絡資訊設定（靜態預設值）
+// 此檔案由後台「聯絡資訊設定」頁面儲存時自動更新
+
+window.IPCC_CONTACT_CONFIG = {
+  "phone0800": "0800-666-080",
+  "phoneHours": "週一至週五 09:00 - 18:00",
+  "lineUrl": "https://lin.ee/rp9TNMU",
+  "recruitUrl": "https://www.1111.com.tw/corp/8563"
+};
